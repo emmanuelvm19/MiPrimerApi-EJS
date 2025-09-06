5afda9245c86083f6e72ec400537ca6e22582e76
Nandez19 <juanpaho2005@gmail.com>
2025-09-06 01:35:05 -0500

    Feat: Se agrega la estructura de prestamos y los endpoints de get

--- Apis/main.py
+++ Apis/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel, Field
 from typing import List, Optional
+from datetime import date
 
 app = FastAPI(
     title="API Biblioteca",
@@ -16,6 +17,8 @@ app = FastAPI(
     ]
 )
 
+
+#USUARIOS
 class Usuario(BaseModel):
     cedula: int = Field(..., example=1)
     nombre: str = Field(..., example="Juan Pablo")
@@ -120,7 +123,7 @@ def eliminar_usuario(cedula: int) -> dict:
 
 
 
-
+#LIBROS
 class libro:
     codigo: float = Field(..., example=1)
     titulo: str = Field(..., example="Cien años de soledad")
@@ -129,14 +132,90 @@ class libro:
     precio: float = Field(..., example = 50000)
 
 libro_db: List[libro] = []
+
+
+
+
+#PRESTAMOS
+
+class Prestamo(BaseModel):
+    idPrestamo: int = Field(..., examples = [1])
+    cedulaUsuario: int = Field(..., examples = [101])
+    idLibro: int = Field(..., examples = [1001])
+    fechaPrestamo: date = Field(..., examples = ["2024-09-05"])
+    fechaDevolucion: Optional[date] = Field(None, examples = ["2025-09-05"])
+    estadoPrestamo: str = Field(..., examples=["Activo", "Devuelto", "Atrasado"])
+
+prestamos_db: List[Prestamo] = [
+    Prestamo(idPrestamo=1, cedulaUsuario=1, idLibro=1001, fechaPrestamo=date(2025, 9, 5), fechaDevolucion= None, estadoPrestamo="Activo"),
+    Prestamo(idPrestamo=2, cedulaUsuario=2, idLibro=1001, fechaPrestamo=date(2025, 8, 5), fechaDevolucion= date(2025, 9, 5), estadoPrestamo="Devuelto")
+]
+
+#ENDPOINTS PRESTAMOS
+
+@app.get("/prestamos",
+         response_model=list[Prestamo],
+         summary="Obtener lista de todos los prestamos",
+         description="Retorna todos los prestamos realizados en la biblioteca. Permite filtrar por estado del prestamos (Activo, Devuelto, Atrasado).",
+         tags=["Prestamos"],
+            responses={
+                200: {"descripción": "Lista de prestamos obtenida exitosamente."},
+                404: {"descriptión": "No se encontraron prestamos."}
+            } 
+        )
+
+def obtener_trasacciones (estado_prestamo: Optional[str] =
+                          Query(None, description="Filtrar por tipo de prestamo (Activo, Devuelto, Atrasado)")):
+    if estado_prestamo:
+        prestamos_filtrados = [p for p in prestamos_db if p.estadoPrestamo.lower() == estado_prestamo.lower()]
+        if not prestamos_filtrados:
+            raise HTTPException(status_code=404, detail=f"No se encontraron transacciones con tipo: {estado_prestamo}")
+        return prestamos_filtrados
+    return prestamos_db
+#Para obtener todas las trasacciones /prestamos
+#Para obtener todas las trasacciones con un estado especifico /prestamos?estado_prestamo=Activo
+
+@app.get("/prestamos/{id_prestamo}",
+         response_model=Prestamo,
+         summary="Obtener los datos de un prestamo especifico",
+         description="Retorna la información de un prestamo. Permite filtrar por el ID del prestamo.",
+         tags=["Prestamos"],
+            responses={
+                200: {"descripción": "Datos de prestamo obtenido exitosamente."},
+                404: {"descriptión": "No se encontraron prestamo especifico."}
+            }
+         )
+
+
+def obtener_prestamo(id_prestamo: int):
+    for prestamo in prestamos_db:
+        if prestamo.idPrestamo == id_prestamo:
+            print("DEBUG →", prestamo)
+            return prestamo
+    raise HTTPException(status_code=404, detail="Prestamo no encontrado")
+#Para obtener el registro de un prestamos con un id especifico /prestamos/1
+
+
+
+
+
+
     
 #Ruta raiz
 @app.get("/")
 def read_root():
-    return {"Bienvenida": "Hola, bienvenido a biblioteca (realizada con API rest)"}
+    return {"Bienvenido": " API de Biblioteca",
+            "version" : "1.0.0",
+            "endpoints" : {
+                "usuarios":"/usuarios",
+                "libros":"/libros",
+                "prestamos":"/prestamos",
+                "documentacion swagger":"/docs"
+            }}
+
 
 #Ruta con parametro
-@app.get("/items/{item_id}")
-def read_item(libro_cod: int, q: str = None):
-    return {"item_id": libro_cod, "Titulo": q}
+#@app.get("/items/{item_id}")
+#def read_item(libro_cod: int, q: str = None):
+   # return {"item_id": libro_cod, "Titulo": q}
 
